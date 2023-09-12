81be1b19f527b3a21dac2f7cb7e406d9913e37d0
PietervdWerk <32298501+PietervdWerk@users.noreply.github.com>
2023-09-12 11:40:19 +0200

    Initial commit

---
+++ src/components/Details.vue
@@ -0,0 +1,163 @@
+<script lang="ts" setup>
+import { PropType, ref } from 'nativescript-vue';
+import HeaderRestaurant from './HeaderRestaurant.vue';
+import DishItem from './DishItem.vue';
+import TopDishCollection from './TopDishCollection.vue';
+import { Restaurant, Dish, ItemViewRestaurant, CategoryItem } from '~/types';
+import type { CollectionView } from '@nativescript-community/ui-collectionview';
+import { Label, ScrollView, View, isIOS } from '@nativescript/core';
+
+const getAllCategories = (dishes: Dish[]) => [...new Set(dishes.map((dish) => dish.type))].sort();
+const itemTemplate = (args: { item: ItemViewRestaurant }) => args.item.typeItem;
+
+const { restaurant, index } = defineProps({
+  restaurant: {
+    type: Object as PropType<Restaurant>,
+    required: true
+  },
+  index: Number
+})
+
+const collectionViewRef = ref();
+const headerCategoriesRef = ref();
+const headerRef = ref();
+const categories = ref<CategoryItem[]>(getAllCategories(restaurant.dishes).map<CategoryItem>(category => ({ name: category, typeItem: "category", hasFocus: false, index: 0 })))
+
+const dishes = restaurant.dishes.map<Dish & { typeItem: string }>((dish) => ({ ...dish, ...{ typeItem: "food", description: dish.description.split(" ").slice(0, 15).join(" ") + "..." } }))
+const items: ItemViewRestaurant[] = [{ typeItem: "header" }, { typeItem: "top" }];
+let animatingHeaderCategory = false;
+let enableWatchHeaderCategory = true;
+let currentCategory = -1;
+let offsetFirstCategory = 2;
+
+buidItemsList();
+
+function buidItemsList() {
+  let indexCategorySum = 0;
+  categories.value.forEach((category, i) => {
+    category.index = i + indexCategorySum;
+    items.push(category);
+
+    const dishesOnCategory = dishes.filter(dishe => dishe.type === category.name);
+    items.push(...dishesOnCategory);
+
+    indexCategorySum += dishesOnCategory.length
+  })
+}
+
+const onScroll = (args: { object: CollectionView, scrollOffset: number }) => {
+  const headerView: View = headerRef.value.nativeView;
+  if (args.scrollOffset > 200 && headerView.visibility != "visible" && animatingHeaderCategory == false) {
+    animatingHeaderCategory = true;
+    headerView.visibility = "visible";
+    headerView.animate({
+      opacity: 1,
+      duration: 300,
+      translate: {
+        y: 0,
+        x: 0
+      }
+    }).then(() => {
+      animatingHeaderCategory = false;
+    })
+  } else if (args.scrollOffset < 200 && headerView.opacity == 1 && animatingHeaderCategory == false) {
+    animatingHeaderCategory = true;
+    headerView.animate({
+      opacity: 0,
+      duration: 300,
+      translate: {
+        y: -headerView.getActualSize().height,
+        x: 0
+      }
+    }).then(() => {
+      headerView.visibility = "collapse";
+      animatingHeaderCategory = false;
+    })
+  }
+  if (enableWatchHeaderCategory)
+    updateTabsPosition(args.object);
+}
+
+function updateTabsPosition(collectionView: CollectionView, forcePosition = -1) {
+  const actualVisibleCategories = categories.value.map((category) => forcePosition == -1 ? (collectionView.isItemAtIndexVisible(category.index)) : category.index === forcePosition);
+
+
+  let firstVisible = actualVisibleCategories.findIndex(visibleCategory => visibleCategory);
+  firstVisible = firstVisible == 0 ? 0 : forcePosition == -1 ? firstVisible - 1 : firstVisible;
+  if (firstVisible != -1 && currentCategory != firstVisible) {
+    currentCategory = firstVisible;
+    categories.value.forEach(c => c.hasFocus = false);
+    categories.value[firstVisible].hasFocus = true;
+
+    // Move ScrollView
+    const headerCategoriesView = headerCategoriesRef.value.nativeView as View;
+    headerCategoriesView.eachChildView((child) => {
+      if ((child as Label).text === categories.value[currentCategory].name) {
+        (headerCategoriesView.parent as ScrollView).scrollToHorizontalOffset(child.getLocationRelativeTo(headerCategoriesView).x, true);
+      }
+      return true;
+    })
+  }
+}
+
+function goToCategory(category: CategoryItem) {
+  enableWatchHeaderCategory = false;
+  const collectionView: CollectionView = collectionViewRef.value.$el.nativeView;
+  const indexInCollectionView = category.index + offsetFirstCategory;
+
+  collectionView.scrollToIndex(indexInCollectionView - 1, true);
+  setTimeout(() => {
+    updateTabsPosition(collectionView, category.index);
+    enableWatchHeaderCategory = true;
+  }, isIOS ? 350 : 100);
+}
+
+</script>
+
+<template>
+  <Page actionBarHidden="true" androidStatusBarBackground="white">
+    <GridLayout class="bg-white">
+      <CollectionView ref="collectionViewRef" iosOverflowSafeArea="true" verticalAlignment="top"
+        separatorColor="transparent" :itemTemplateSelector="itemTemplate" :items="items" colWidth="100%" height="100%"
+        @scroll="onScroll">
+        <template #header="{ item, index }">
+          <StackLayout class="">
+            <HeaderRestaurant :restaurant="restaurant" :index="index"></HeaderRestaurant>
+          </StackLayout>
+        </template>
+        <template #category="{ item, index }">
+          <StackLayout class="">
+            <Label class="text-2xl mt-4 px-3" :text="item.name"></Label>
+          </StackLayout>
+        </template>
+        <template #food="{ item, index } : { item: Dish, index: number } ">
+          <StackLayout class="">
+            <DishItem :dish=" item " :index=" index "></DishItem>
+          </StackLayout>
+        </template>
+        <template #top=" { item, index } : { item: Dish, index: number } ">
+          <StackLayout class="">
+            <Label class="mt-3 text-2xl pl-3" text="Featured articles"></Label>
+            <TopDishCollection :restaurant=" restaurant "></TopDishCollection>
+          </StackLayout>
+        </template>
+      </CollectionView>
+
+      <!-- HEADER CATEGORIES -->
+      <StackLayout ref="headerRef" translateY="-300" verticalAlignment="top" class="bg-white" opacity="0"
+        visibility="collapse">
+        <Label textWrap="true" class="font-bold text-xl px-3 mt-2" :text=" restaurant.name "></Label>
+        <GridLayout height="60" width="100%" class="">
+          <ScrollView orientation="horizontal" scrollBarIndicatorVisible="false">
+            <StackLayout orientation="horizontal" ref="headerCategoriesRef">
+              <Label v-for="( category, index ) in categories" :key=" index "
+                :text=" category.name " class="px-3 py-1" @tap="goToCategory(category)" style="border-bottom-width: 6"
+                :class=" [category.hasFocus === true ? 'border-b-black' : 'border-b-gray-300'] ">
+              </Label>
+            </StackLayout>
+          </ScrollView>
+        </GridLayout>
+      </StackLayout>
+    </GridLayout>
+  </Page>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,184 @@
+<script lang="ts" setup>
+import {
+  $navigateTo,
+  $showModal,
+  ref
+} from 'nativescript-vue';
+import Header from './Header.vue';
+import TextSearch from './TextSearch.vue';
+import Details from './Details.vue';
+import CarItemRestaurant from './CarItemRestaurant.vue';
+import { items } from '~/MockData';
+import { View, Utils, isIOS, SharedTransition, PageTransition, ModalTransition, Screen, SharedTransitionConfig } from '@nativescript/core';
+import CarItemOption from './CarItemOption.vue';
+import type { CollectionView } from '@nativescript-community/ui-collectionview';
+import { DataItem, Restaurant } from '~/types';
+import { onScrollDirection } from '~/ScrollUtil';
+
+const headerViewRef = ref();
+
+const durationHeaderAnimation = 250;
+let heightHeader = 0;
+let animating = false;
+let collapsed = false;
+
+function loadedCollectionView(args: { object: CollectionView }) {
+  const header = headerViewRef.value.nativeView as View;
+  heightHeader = 0;
+  header.eachChildView((child) => {
+    const marginBottom = Utils.isNumber(child.marginBottom) ? child.marginBottom : child.marginBottom?.value ? child.marginBottom.value : 0;
+    const marginTop = Utils.isNumber(child.marginTop) ? child.marginTop : child.marginTop?.value ? child.marginTop.value : 0;
+    heightHeader += child.getActualSize().height + marginBottom + marginTop
+    return true
+  })
+  args.object.translateY = heightHeader + 5
+}
+
+const itemTemplate = (args: { item: DataItem }) => {
+  return args.item.type;
+}
+
+const animateTranslateY = (view: View, y: number, opacity = 1) => view.animate({
+  translate: {
+    y,
+    x: 0
+  },
+  opacity,
+  duration: durationHeaderAnimation
+})
+
+
+const onScroll = (args: { object: CollectionView, scrollOffset: number }) => {
+  const collection = args.object;
+
+  onScrollDirection(args, {
+    onTop: () => {
+      if (!animating && collapsed) {
+        const view = (headerViewRef.value.nativeView as View);
+        animating = true;
+        collapsed = false;
+        animating = true;
+        animateTranslateY(collection, heightHeader + 5)
+        animateTranslateY(view, 0, 1).then(() => {
+          animating = false;
+        })
+      }
+    }, onBottom: () => {
+      const view = (headerViewRef.value.nativeView as View);
+      if (!animating && view.translateY == 0) {
+        animating = true;
+        collapsed = true;
+        animateTranslateY(collection, 0)
+        animateTranslateY(view, - heightHeader + 5, 0).then(() => {
+          animating = false;
+        })
+
+      }
+    }
+  })
+}
+
+function goTo(restaurant: Restaurant, index: number) {
+
+
+  if (isIOS) {
+    $showModal(Details, {
+      transition: SharedTransition.custom(new PageTransition()),
+      props: {
+        restaurant,
+        index
+      },
+      fullscreen: true
+
+    })
+  } else {
+
+    $navigateTo(Details, {
+      transition: SharedTransition.custom(new PageTransition(), {
+        pageStart: {
+          x: 0,
+          y: 300,
+          // Try commenting above values and uncommenting these...
+          // x: 0,
+          // y: -300,
+        },
+        pageEnd: {
+          // use nice linear duration on Android
+          duration: 500,
+          // use custom spring on iOS
+          spring: { tension: 60, friction: 8, mass: 4 },
+          opacity: 1,
+
+        },
+        pageReturn: {
+          duration: 500,
+          x: -Screen.mainScreen.widthDIPs,
+          y: 0,
+          opacity: 0,
+        },
+        interactive: {
+          dismiss: {
+            finishThreshold: 0.5
+          }
+        }
+      }),
+      props: {
+        restaurant,
+        index
+      }
+
+    })
+  }
+}
+</script>
+
+<template>
+  <Frame>
+    <Page actionBarHidden="true" androidStatusBarBackground="white">
+      <GridLayout rows="auto">
+        <StackLayout ref="headerViewRef" class="px-4 py-1">
+          <Header class="my-2"></Header>
+          <TextSearch class="mt-4  pb-2"></TextSearch>
+        </StackLayout>
+
+        <CollectionView @layoutChanged="loadedCollectionView" @scroll="onScroll" :items="items" colWidth="100%"
+          :itemTemplateSelector="itemTemplate" class="bg-gray" height="100%" 
+          iosOverflowSafeArea="true" ios:marginTop="1">
+          <template #default="{ item, index }">
+            <StackLayout class="android:pb-3 ios:pb-2" @tap="goTo(item, index)">
+              <CarItemRestaurant :restaurant="item" :index="index"></CarItemRestaurant>
+            </StackLayout>
+          </template>
+          <template #option="{ item }">
+            <StackLayout class="android:pb-3 ios:pb-2">
+              <StackLayout class="px-4 android:py-3 ios:py-2 bg-white text-center" orientation="horizontal">
+                <CarItemOption v-for="(option, i) in item.options" :key="i" :index="i" height="95" width="28%"
+                  :item="option"></CarItemOption>
+              </StackLayout>
+            </StackLayout>
+          </template>
+
+          <template #ad="{ item }">
+            <StackLayout class="android:pb-3 ios:pb-2">
+              <Pager transformers="stack" class="bg-white py-2" :height="isIOS ? 185 : 'auto'">
+                <PagerItem v-for="(ad, i) in item.ads" :key="i" :index="i">
+                  <StackLayout class="px-4  text-center">
+                    <FlexboxLayout class="rounded-2xl" :backgroundColor="ad.bg">
+                      <FlexboxLayout width="65%" class="p-2 flex-col justify-between">
+                        <Label :text="ad.title" textWrap="true" style="line-height:1"
+                          class="font-bold text-2xl text-center mt-1 text-black"></Label>
+                        <Label :text="ad.subtitle"
+                          class="font-bold text-base text-center mt-1 text-black bg-white rounded-full"></Label>
+                      </FlexboxLayout>
+                      <NSImg height="150" width="35%" stretch="aspectFill" borderRadius="20" :src="ad?.img"> </NSImg>
+                    </FlexboxLayout>
+                  </StackLayout>
+                </PagerItem>
+              </Pager>
+            </StackLayout>
+          </template>
+        </CollectionView>
+      </GridLayout>
+    </Page>
+  </Frame>
+</template>
